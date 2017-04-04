560dca14af6ffb9c4268fcc3eb764510fa592a06
Igelex <sergatam@yandex.com>
2017-04-04 17:22:54 +0200

    work on AddNewWord Activity

--- app/src/main/java/com/example/android/linote/AddNewWord.java
+++ app/src/main/java/com/example/android/linote/AddNewWord.java
@@ -2,6 +2,8 @@ package com.example.android.linote;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddNewWord extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class AddNewWord extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_new_word_activity);
+
+        Spinner spinner = (Spinner) findViewById(R.id.chose_lang_spinner);
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.chose_lang_spinner, android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+
     }
 }
