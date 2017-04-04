e10862b5c6b2768dab95faa169d534cdab888d48
Igelex <sergatam@yandex.com>
2017-04-04 20:48:28 +0200

    Implement AddNewWord Activity

--- app/src/main/java/com/example/android/linote/AddNewWord.java
+++ app/src/main/java/com/example/android/linote/AddNewWord.java
@@ -1,8 +1,14 @@
 package com.example.android.linote;
 
 import android.os.Bundle;
+import android.support.annotation.StringDef;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 
 public class AddNewWord extends AppCompatActivity {
@@ -12,10 +18,48 @@ public class AddNewWord extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_new_word_activity);
 
-        Spinner spinner = (Spinner) findViewById(R.id.chose_lang_spinner);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.main_relative_layout);
+
+        Spinner spinnerChoseLang = (Spinner) findViewById(R.id.chose_lang_spinner);
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this,
                 R.array.chose_lang_spinner, android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(spinnerAdapter);
+        spinnerChoseLang.setAdapter(spinnerAdapter);
+
+        Spinner spinnerChosePos = (Spinner) findViewById(R.id.spinner_partofspeech);
+        ArrayAdapter<CharSequence> spinnerAdapterPos = ArrayAdapter.createFromResource(this,
+                R.array.chose_pos_spinner, android.R.layout.simple_spinner_dropdown_item);
+        spinnerChosePos.setAdapter(spinnerAdapterPos);
+
+        Spinner spinnerChoseArticle = (Spinner) findViewById(R.id.spinner_article);
+        ArrayAdapter<CharSequence> spinnerAdapterArticle = ArrayAdapter.createFromResource(this,
+                R.array.chose_article_spinner, android.R.layout.simple_spinner_dropdown_item);
+        spinnerChoseArticle.setAdapter(spinnerAdapterArticle);
+
+        spinnerChoseLang.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+
+                if(selection.equals(getString(R.string.page_title_eng))){
+                    Snackbar.make(view, "Item on Position :" + position + " ist Selected: " + selection, Snackbar.LENGTH_SHORT).show();
+                }
+                switch (position){
+                    case 0:
+                        Snackbar.make(view, "Item Selected: " + selection, Snackbar.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Snackbar.make(view, "Item Selected: " + selection, Snackbar.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Snackbar.make(view, "Item Selected: " + selection, Snackbar.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
 
     }
 }
